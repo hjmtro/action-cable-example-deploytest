cb9bbdb7c6e0ccf01829f7eae2ba3f8081cffe77
寺尾 啓 <hajime.terao@p757.wn.oro.co.jp>
2016-05-11 20:56:26 +0900

    added some changes.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'action-cable-example-deploytest'
+set :repo_url, 'git@github.com:hjmtro/action-cable-example-deploytest.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/action-cable-example-deploytest'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -24,16 +24,19 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'run/sockets', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :rbenv_path, '$HOME/.rbenv' # add
+set :rbenv_path, '$HOME/.rbenv' # add
+set :rbenv_ruby, '2.3.0' # add
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
@@ -45,5 +48,27 @@ namespace :deploy do
       # end
     end
   end
+
+  # add
+  desc 'Upload database.yml and secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      #if test "[ ! -d #{shared_path}/config ]"
+      #  execute "mkdir -p #{shared_path}/config"
+      #end
+      upload!('config/database.yml', "#{shared_path}/config/database.yml")
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  after 'deploy:check:make_linked_dirs', 'deploy:upload'
+  #before :starting, 'deploy:upload'
+
+  # add
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+  after 'deploy:publishing', 'deploy:restart'
+
 
 end
